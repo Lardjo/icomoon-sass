6f8d73740f7bfa450265184ff2fef2afbdf8d332
Lardjo <git@statsmile.com>
2014-12-31 16:37:54 +0300

    Added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    notify = require('gulp-notify');
+
+var config = {
+    scssDir: './assets/scss'
+};
+
+gulp.task('css', function () {
+    return gulp.src(config.scssDir + '/icomoon.scss')
+        .pipe(sass({
+            style: 'expanded',
+            sourcemap: false
+        }))
+        .on('error', notify.onError(function (error) {
+            return 'Error: ' + error.message;
+        }))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', 'css');
